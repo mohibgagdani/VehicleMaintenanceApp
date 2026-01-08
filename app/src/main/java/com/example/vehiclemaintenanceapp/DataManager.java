82124e2c6e28427f6b9e17589f1c5c25cfdcd021
salmankazi53 <skazi1143@gmail.com>
2026-01-08 14:44:21 +0530

    first commit

--- app/src/main/java/com/example/vehiclemaintenanceapp/DataManager.java
+++
@@ -1,9 +0,0 @@
-package com.example.vehiclemaintenanceapp;
-
-import android.content.Context;
-import android.content.SharedPreferences;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.util.ArrayList;
-import java.util.List;
