a8b4b52155fa3ff7112c9765a1c6e87325ebd374
salmankazi53 <skazi1143@gmail.com>
2026-01-10 14:33:00 +0530

    2nd commit

--- app/src/main/java/com/example/vehiclemaintenanceapp/DataManager.java
+++ app/src/main/java/com/example/vehiclemaintenanceapp/DataManager.java
@@ -7,13 +7,3 @@ import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.ArrayList;
 import java.util.List;
-public class DataManager {
-    private static final String PREFS_NAME = "VehicleMaintenancePrefs";
-    private static final String KEY_VEHICLES = "vehicles";
-    private static final String KEY_THEME = "theme_mode";
-    private SharedPreferences prefs;
-
-    public DataManager(Context context) {
-        prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-    }
-}
